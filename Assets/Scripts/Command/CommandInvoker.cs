67d8e78a1ce022ba151693e081034c5da90a1719
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2025-03-11 13:03:33 -0400

    Create Command Invoker

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,13 @@
+using Commands;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker
+{
+    private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+
+    public void ExecuteCommand(ICommand command) => command.Execute();
+
+    public void RegisterCommand(ICommand command) => commandRegistry.Push(command);
+}
